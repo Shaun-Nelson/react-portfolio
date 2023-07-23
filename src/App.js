909398d0f58f4c9435a179138b3b661884ef8a4b
Shaun Nelson <shaunptnelson@gmail.com>
2023-07-23 16:43:07 -0400

    Created basic page layout and finished About page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import About from "./components/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./styles.css";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+      <About />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className='container-content'>
+      <h2>About Me</h2>
+      <img src='' alt='' />
+      <article>
+        <p>
+          Hi, I'm Shaun. I'm an aspiring full stack web developer with a
+          background in biochemistry and digital marketing. I'm passionate about
+          creating applications that are accessible to everyone. I'm currently
+          looking for a position as a full stack web developer.
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default About;
