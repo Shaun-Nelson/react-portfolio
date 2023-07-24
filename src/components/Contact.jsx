2dee0aeb1d4a1203f49a7f0aec267707483489f6
Shaun Nelson <shaunptnelson@gmail.com>
2023-07-24 12:33:11 -0400

    Added Contact form and page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='container-content'>
+      <h2>Contact Me</h2>
+      <div className='container-flex'>
+        <form>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' id='name' />
+          <label htmlFor='email'>Email:</label>
+          <input type='email' id='email' required />
+          <label htmlFor='message'>Message:</label>
+          <textarea id='message' rows='5'></textarea>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
