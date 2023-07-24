599521ac3603f096bfe78c94b455a7d94f92a2d7
Shaun Nelson <shaunptnelson@gmail.com>
2023-07-24 06:48:33 -0400

    Added Projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,24 @@ const Projects = () => {
     <>
       <div className='container-content'>
         <h1>Projects</h1>
+        <div className='container-grid'>
+          <div class='project'>
+            <a href='https://shaun-nelson.github.io/UFO-Tracker/'>
+              <figure>
+                <img
+                  src={require("../assets/ufo.jpeg")}
+                  alt='Flying saucer with light coming out of bottom'
+                ></img>
+              </figure>
+            </a>
+            <div class='project-headings'>
+              <a href='https://shaun-nelson.github.io/UFO-Tracker/'>
+                <h3>FindU (UFO Tracker)</h3>
+                <h6>JS/Bootstrap/APIs</h6>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
